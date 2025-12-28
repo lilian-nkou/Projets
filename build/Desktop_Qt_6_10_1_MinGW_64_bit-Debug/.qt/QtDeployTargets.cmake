1bfaa78659afd8d15c50f90d35f6be8b432ee8f4
lilian-nkou <liliannkou2@gmail.com>
2025-12-28 14:48:58 +0100

    version test1

---
+++ build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_verite_FILE C:/verite/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/verite.exe)
+set(__QT_DEPLOY_TARGET_verite_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_verite_RUNTIME_DLLS C:/Qt/6.10.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Core.dll)
